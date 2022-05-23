ecd47db6195c104b4c3d146ac769b74ed27527e5
Nisflei <ncgaloni@hotmail.com>
2022-05-23 20:48:07 -0300

    HerokuFinal

--- src/main/java/com/petcare/controller/PetCareController.java
+++ src/main/java/com/petcare/controller/PetCareController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -40,8 +41,42 @@ public class PetCareController {
 	
 	@PostMapping("/formularioNovo")
 	public String formularioNovo(PetCare requisicao) {
+		requisicao.setStatus("Pendente");
 		petcareRepository.save(requisicao);
 		return "redirect:/listaDados";
 	}
 	
+	@GetMapping("/update/{id}/{status}")
+	public String update(@PathVariable Long id, @PathVariable String status) {
+		
+		PetCare pet = petcareRepository.getById(id);
+		pet.setStatus(status);
+		
+		petcareRepository.save(pet);
+		
+		//return "redirect:/listaDados";  ## alterado para abrir o novo template
+		return "redirect:/index";
+	}
+	
+	// =========== usando Template Freee
+	
+	@GetMapping("/index")
+	public String index(Model request) {
+		List<PetCare> lista = petcareRepository.findAll();
+		request.addAttribute("listaDados", lista);	
+		return "/anipat-master/index";
+	}
+	
+	@GetMapping("/denuncia")
+	public String denuncia() {
+		return "/anipat-master/denuncia";
+	}
+	
+	@PostMapping("/denunciaNovo")
+	public String denunciaNovo(PetCare requisicao) {
+		requisicao.setStatus("Pendente");
+		petcareRepository.save(requisicao);
+		return "redirect:/index";
+	}
+
 }
